2d2704f24cf5898ef31500509e065f3f81fad70d
Patrick.Dutto <patrick.dutto@itiscuneo.eu>
2024-10-10 13:26:25 +0200

    aggiornamenti clien e server
    nuova funzione non totalmente completata per far muovere il robot con wasd e una nuova libreria pynput

--- client_per_robot.py
+++ client_per_robot.py
@@ -1,7 +1,48 @@
 import socket
 #lista e fai elif x vane 
-SERVER_ADDRESS = ("192.168.1.119", 9090)
+SERVER_ADDRESS = ("192.168.1.123", 9999)
 BUFFER_SIZE = 4096
+from pynput import keyboard
+
+def setMotor(self, left, right):
+		if((right >= 0) and (right <= 100)):
+			GPIO.output(self.IN1,GPIO.HIGH)
+			GPIO.output(self.IN2,GPIO.LOW)
+			self.PWMA.ChangeDutyCycle(right)
+		elif((right < 0) and (right >= -100)):
+			GPIO.output(self.IN1,GPIO.LOW)
+			GPIO.output(self.IN2,GPIO.HIGH)
+			self.PWMA.ChangeDutyCycle(0 - right)
+		if((left >= 0) and (left <= 100)):
+			GPIO.output(self.IN3,GPIO.HIGH)
+			GPIO.output(self.IN4,GPIO.LOW)
+			self.PWMB.ChangeDutyCycle(left)
+		elif((left < 0) and (left >= -100)):
+			GPIO.output(self.IN3,GPIO.LOW)
+			GPIO.output(self.IN4,GPIO.HIGH)
+			self.PWMB.ChangeDutyCycle(0 - left)
+
+#funzione chiamata quando un tasto viene premuto 
+def on_press(key):
+    #try
+    if key.char == "w":
+        print("press w")
+# expect AttributeError:
+# ignora tasti speciali come Shift, Crel, etc
+#pass
+#funzione chiamata quando un tasto viene rilasciato 
+def on_release(key):
+    #try:
+    if key.char == "w":
+        print("release w")
+#except AttributeError:
+#       pass
+def start_listener():
+    #Listener per intercettare gli eventi da tastiera 
+    with keyboard.listener(on_press = on_press, on_release = on_release) as listener:
+            listener.join()
+
+start_listener()
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
