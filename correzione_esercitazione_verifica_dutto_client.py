13ab3434836a047602591f228b24cd69166b0fea
Patrick.Dutto <patrick.dutto@itiscuneo.eu>
2024-11-25 11:08:36 +0100

    correzione database
    commento correzione verifica

--- correzione_esercitazione_verifica_dutto_client.py
+++ correzione_esercitazione_verifica_dutto_client.py
@@ -1,60 +1,68 @@
-import sqlite3
 import socket
 
-# Impostazioni server
-MY_ADDRESS = ("127.0.0.1", 9090)
-NETWORK = "192.168.0.0/27"
+SERVER_ADDRESS = ("127.0.0.1", 9090)
 BUFFER_SIZE = 4096
-COMMON_PORTS = [22, 80, 443, 21, 25, 53, 110, 143, 3306, 3389]
-
-def scansione_host():
-    open_ports = []
-    try:
-        host_name = socket.gethostbyaddr(ip)[0]
-    except socket.herror:
-        host_name = None
-
-    for port in COMMON_PORTS:
-        sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-
-def scansione_network():
-    threads=[]
-    for ip in ip_network(NETWORK).hosts():
-        thread = threading.Thread(target=scan_host, args=(str(ip),))
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-def createdatabase():
-    conn = sqlite3.connect("ip_list.db")
-    cur = conn.cursor()
-
-    query = """CREATE TABLE IF NOT EXIST host(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                ip_host INTAGER,
-                nome_host VARCHAR(20)
-                port_list INTAGER
-                )"""
-    cur.execute(query)
-    cur.execute("INSERT INTO ip_list(ip_host, nome_host, port_list) VALUES (?, ?, ?)",
-                (ip, host_name.ù, ','.join(map(str, open_port))))
-    conn.commit()
-    conn.close()
-
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(MY_ADDRESS)
-    s.listen()
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(SERVER_ADDRESS)
+        print("Connesso al server.")
+        
+        while True:
+            print("\nScegli un'azione:")
+            print("1. Controlla se un file esiste")
+            print("2. Ottieni il numero di frammenti di un file")
+            print("3. Ottieni l'IP dell'host di un frammento specifico")
+            print("4. Ottieni tutti gli IP degli host di un file")
+            print("5. Esci")
+
+            scelta = input("Scelta: ")
+
+            if scelta =="1":
+                s.sendall("Check_file".encode())
+                message = s.recv(BUFFER_SIZE).decode()
+                filename = input(f"{message} ")
+                s.sendall(filename.encode()) 
+                risposta = s.recv(BUFFER_SIZE).decode()
+
+            elif scelta == "2":
+                s.sendall("get_fragment_count".encode())
+                message = s.recv(BUFFER_SIZE).decode()
+                filename = input(f"{message}")
+                s.sendall(filename.encode())
+                risposta = s.recv(BUFFER_SIZE).decode()
+                print(f"numero frammenti = {risposta}")
+
+            elif scelta == "3":
+                s.sendall("get_fragment_host".encode())
+                message = s.recv(BUFFER_SIZE).decode()
+                filename, fragment_number = input(f"{message} ").split(";")
+                s.sendall(f"{filename};{fragment_number}".encode())
+                risposta = s.recv(BUFFER_SIZE).decode()
+                print("IP dell'host:", risposta)
+                
+            elif scelta == "4":
+                s.sendall("get_all_host".encode())
+                messagio = s.recv(BUFFER_SIZE).decode()
+                file_name = input(f"{messagio}")
+                s.sendall(file_name.encode())
+                risposta = s.recv(BUFFER_SIZE).decode()
+                print("IP degli host:", risposta)
+
+            elif scelta == "5":
+                break
+
+
+            
+    except Exception as e:
+            print(f"Errore: {e}")
     
-    while True:
-        connection, client_address = s.accept()
-        print(f"Il client {client_address} si è connesso")
+    finally:
+        s.close()
+        print("Connessione chiusa.")
 
 
+    
 if __name__ == "__main__":
     main()
-
-
